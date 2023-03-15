937fb153e81d1193cba6160bda1b5350d276e23c
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-15 11:31:32 +0300

    add simple test for marker

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "marker.h"
 
 int main()
 {
     std::cout << "Hello world\n";
+    int first_id = 1;
+    DWORD marker_id;
+    HANDLE marker_handle = CreateThread(NULL, 0, marker, (void*) &first_id, 0, &marker_id);
+    WaitForSingleObject(marker_handle, INFINITE);
     return 0;
 }
