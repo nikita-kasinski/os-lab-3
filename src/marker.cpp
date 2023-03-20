f38c453c77d325b98959bca68b440a7c98ff50e9
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-20 13:30:52 +0300

    add args handling in marker function

--- src/marker.cpp
+++ src/marker.cpp
@@ -1,9 +1,13 @@
 //author: Nikita Kasinski
 #include "windows.h"
 #include <iostream>
+#include "struct_args.h"
 
-DWORD WINAPI marker(LPVOID args)
+DWORD WINAPI marker(LPVOID _args)
 {
-    int id = *(int*)args;
+    MarkerArgs args = *(MarkerArgs*)_args;
+    int id = args.id;
+    int* array = args.array;
+    int n = args.n;
     std::cout << "Create marker with id " << id << "\n";
 }
