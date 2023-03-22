c677649e096e0875a58977bd242c209f75fad457
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-22 11:44:09 +0300

    implement thread control in main

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,13 @@ int main()
     std::cout << "Enter the quantity of markers\n";
     std::cin >> marker_count;
     HANDLE *events = new HANDLE[marker_count];
+    HANDLE *workEvents = new HANDLE[marker_count];
     for (size_t i = 0; i < marker_count; ++i)
     {
-        std::string eventName = "Marker event " + std::to_string(i);
-        std::wstring wEventName = std::wstring(eventName.begin(), eventName.end());
-        events[i] = CreateEvent(NULL, TRUE, TRUE, eventName.c_str());
+        std::string eventName = "Marker event " + std::to_string(i + 1);
+        std::string workEventName = "Work event " + std::to_string(i + 1);
+        events[i] = CreateEvent(NULL, TRUE, FALSE, eventName.c_str());
+        workEvents[i] = CreateEvent(NULL, TRUE, FALSE, workEventName.c_str());
     }
     HANDLE *markers = new HANDLE[marker_count];
     MarkerArgs *args = new MarkerArgs[marker_count];
@@ -41,9 +43,60 @@ int main()
         args[i].finish = finish;
         args[i].iocs = iocs;
         args[i].wcs = wcs;
-        markers[i] = CreateThread(NULL, 0, marker, (void*)(&args[i]), 0, &markerThreadIds[i]);
+        markers[i] = CreateThread(NULL, 0, marker, (void *)(&args[i]), 0, &markerThreadIds[i]);
     }
-    WaitForMultipleObjects(marker_count, markers, TRUE, INFINITE);
+    for (size_t i = 0; i < marker_count; ++i)
+    {
+        WaitForMultipleObjects(marker_count, events, TRUE, INFINITE);
+        std::cout << "Array:\n";
+        for (size_t i = 0; i < n; ++i)
+        {
+            std::cout << array[i] << ' ';
+        }
+        std::cout << "\nIds of working markers: ";
+        for (size_t i = 0; i < marker_count; ++i)
+        {
+            if (finish[i] == 0)
+            {
+                std::cout << i + 1 << ' ';
+            }
+        }
+        std::cout << "\n";
+        bool correct = false;
+        int id = -1;
+        while (!correct)
+        {
+            std::cout << "Enter working marker id :\n";
+            std::cin >> id;
+            id--;
+            if (id < 0)
+            {
+                continue;
+            }
+            if (finish[id] == 0)
+            {
+                correct = true;
+            }
+        }
+        std::cout << "Finish marker " << id + 1 << "\n";
+        finish[id] = 1;
+        SetEvent(workEvents[id]);
+        WaitForSingleObject(markers[id], INFINITE);
+        std::cout << "Array:\n";
+        for (size_t i = 0; i < n; ++i)
+        {
+            std::cout << array[i] << ' ';
+        }
+        std::cout << "\n\n";
+        for (size_t i = 0; i < marker_count; ++i)
+        {
+            if (finish[i] == 0)
+            {
+                ResetEvent(events[i]);
+                SetEvent(workEvents[i]);
+            }
+        }
+    }
     for (size_t i = 0; i < marker_count; ++i)
     {
         CloseHandle(events[i]);
