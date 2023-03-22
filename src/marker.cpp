2a3ca401baf41e65606d98633ecb9f8cb7f024d5
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-22 09:14:35 +0300

    handle critical sections and finish array in struct args

--- src/marker.cpp
+++ src/marker.cpp
@@ -9,5 +9,11 @@ DWORD WINAPI marker(LPVOID _args)
     int id = args.id;
     int* array = args.array;
     int n = args.n;
+    int* finish = args.finish;
+    CRITICAL_SECTION iocs = *args.iocs;
+    CRITICAL_SECTION wcs = *args.wcs;
+    std::string eventName = "Marker event " + std::to_string(id);
+    HANDLE event = CreateEvent(NULL, TRUE, TRUE, eventName.c_str());
     std::cout << "Create marker with id " << id << "\n";
+    //SetEvent(event);
 }
