0783310b9b0aa3800c6c04a06ef43d35e0c9a4f3
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-03-22 10:03:53 +0300

    implement marker functionality

--- src/marker.cpp
+++ src/marker.cpp
@@ -2,6 +2,7 @@
 #include "windows.h"
 #include <iostream>
 #include "struct_args.h"
+#include <random>
 
 DWORD WINAPI marker(LPVOID _args)
 {
@@ -16,7 +17,39 @@ DWORD WINAPI marker(LPVOID _args)
     std::cout << "Create marker with id " << id << "\n";
     LeaveCriticalSection(iocs);
     std::string eventName = "Marker event " + std::to_string(id);
-    //HANDLE event = CreateEvent(NULL, TRUE, TRUE, eventName.c_str());
-    //SetEvent(event);
-    return 1;
+    HANDLE event = CreateEvent(NULL, TRUE, TRUE, eventName.c_str());
+
+    std::mt19937 rand(id);
+    while(true)
+    {
+        int pos = rand() % n;
+        EnterCriticalSection(wcs);
+        if (array[pos] == 0)
+        {
+            Sleep(5);
+            array[pos] = id;
+            Sleep(5);
+            LeaveCriticalSection(wcs);
+        }
+        else
+        {
+            LeaveCriticalSection(wcs);
+            SetEvent(event);
+            WaitForSingleObject(event, INFINITE);
+            if (finish[id] == 1)
+            {
+                EnterCriticalSection(wcs);
+                for (size_t i = 0; i < n; ++i)
+                {
+                    if (array[i] == id)
+                    {
+                        array[i] = 0;
+                    }
+                }
+                LeaveCriticalSection(wcs);
+                break;
+            }
+        }
+    }
+    return 0;
 }
