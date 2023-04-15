2d35a74d71777ca6a9f5eeaebdf4220213fbc22a
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 19:50:29 +0300

    add utility class and declare initArray and readPositiveInteger methods

---
+++ include/utility.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+class Utility
+{
+private:
+    static constexpr int valueToInitialize = 0; 
+public:
+    // initializes array with valueToInitialize
+    static void initializeArray(int* array, size_t size);
+
+    // reads integer from in, puts it into destination. If something went wrong returns false
+    static bool readPositiveInteger(
+        std::istream& in, 
+        std::ostream& out,
+        size_t& destination, 
+        const std::string& inputPrompt, 
+        const std::string& inputFailedPrompt);
+
+};
