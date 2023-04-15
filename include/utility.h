837c069eb13c77d0f1a3e069a15b7620083b509f
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 20:20:22 +0300

    declare inputWorkingMarkerId method

--- include/utility.h
+++ include/utility.h
@@ -6,22 +6,32 @@
 class Utility
 {
 private:
-    static constexpr int valueToInitialize = 0; 
+    static constexpr int valueToInitialize = 0;
+
 public:
     // initializes array with valueToInitialize
-    static int* initializeArray(size_t size);
+    static int *initializeArray(size_t size);
 
     // reads integer from in, puts it into destination. If something went wrong returns false
     static bool readPositiveInteger(
-        std::istream& in, 
-        std::ostream& out,
-        size_t& destination, 
-        const std::string& inputPrompt, 
-        const std::string& inputFailedPrompt);
+        std::istream &in,
+        std::ostream &out,
+        size_t &destination,
+        const std::string &inputPrompt,
+        const std::string &inputFailedPrompt);
 
     // outputs array to out
-    static void outputArray(std::ostream& out, const int* array, size_t size);
+    static void outputArray(std::ostream &out, const int *array, const size_t& size);
 
     // outputs working markers
-    static void outputMarkers(std::ostream& out, const int* finish, size_t markerCount);
+    static void outputMarkers(std::ostream &out, const int *finish, const size_t& markerCount);
+
+    // inputs working marker id
+    static size_t inputWorkingMarkerId(
+        std::istream &in, 
+        std::ostream &out, 
+        const int* finish, 
+        const size_t& size, 
+        const std::string inputPrompt);
+
 };
