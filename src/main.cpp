c6ca6d2b3c27d6f9926021b045dab945e7444014
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 20:24:23 +0300

    refactor main to use utility methods, add comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,101 +3,110 @@
 #include <string.h>
 #include "marker.h"
 #include "struct_args.h"
+#include "utility.h"
 
 int main()
 {
-    int n;
-    std::cout << "Enter array size:\n";
-    std::cin >> n;
-    int *array = new int[n];
-    for (size_t i = 0; i < n; ++i)
+    size_t size, markerCount;
+
+    // reading size
+    if (!Utility::readPositiveInteger(
+            std::cin,
+            std::cout,
+            size,
+            "Enter array size\n",
+            "Array size must be positive integer. Abort\n"))
     {
-        array[i] = 0;
+        return 1;
     }
-    int marker_count = 0;
-    std::cout << "Enter the quantity of markers\n";
-    std::cin >> marker_count;
-    HANDLE *events = new HANDLE[marker_count];
-    HANDLE *workEvents = new HANDLE[marker_count];
-    for (size_t i = 0; i < marker_count; ++i)
+
+    // reading markerCount
+    if (!Utility::readPositiveInteger(
+            std::cin,
+            std::cout,
+            markerCount,
+            "Enter the quantity of markers\n",
+            "Marker count must be positive integer. Abort\n"))
+    {
+        return 2;
+    }
+
+    // initializing array
+    int *array = Utility::initializeArray(size);
+
+    // initialize events
+    HANDLE *events = new HANDLE[markerCount];
+    HANDLE *workEvents = new HANDLE[markerCount];
+    for (size_t i = 0; i < markerCount; ++i)
     {
         std::string eventName = "Marker event " + std::to_string(i + 1);
         std::string workEventName = "Work event " + std::to_string(i + 1);
-        events[i] = CreateEvent(NULL, TRUE, FALSE, eventName.c_str());
-        workEvents[i] = CreateEvent(NULL, TRUE, FALSE, workEventName.c_str());
+        events[i] = CreateEventA(NULL, TRUE, FALSE, eventName.c_str());
+        workEvents[i] = CreateEventA(NULL, TRUE, FALSE, workEventName.c_str());
     }
-    HANDLE *markers = new HANDLE[marker_count];
-    MarkerArgs *args = new MarkerArgs[marker_count];
-    unsigned long int *markerThreadIds = new unsigned long int[marker_count];
-    int *finish = new int[marker_count];
+
+    // initialize critical section
     CRITICAL_SECTION *iocs = new CRITICAL_SECTION;
     CRITICAL_SECTION *wcs = new CRITICAL_SECTION;
     InitializeCriticalSection(iocs);
     InitializeCriticalSection(wcs);
-    for (size_t i = 0; i < marker_count; ++i)
+
+    // initialize markers
+    HANDLE *markers = new HANDLE[markerCount];
+    MarkerArgs *args = new MarkerArgs[markerCount];
+    unsigned long int *markerThreadIds = new unsigned long int[markerCount];
+    int *finish = new int[markerCount];
+
+    for (size_t i = 0; i < markerCount; ++i)
     {
         finish[i] = 0;
         args[i].id = i;
-        args[i].n = n;
+        args[i].size = size;
         args[i].array = array;
         args[i].finish = finish;
         args[i].iocs = iocs;
         args[i].wcs = wcs;
         markers[i] = CreateThread(NULL, 0, marker, (void *)(&args[i]), 0, &markerThreadIds[i]);
     }
-    for (size_t i = 0; i < marker_count; ++i)
+
+    // main cycle
+    for (size_t i = 0; i < markerCount; ++i)
     {
-        WaitForMultipleObjects(marker_count, events, TRUE, INFINITE);
+        WaitForMultipleObjects(markerCount, events, TRUE, INFINITE);
+
         std::cout << "Array:\n";
-        for (size_t i = 0; i < n; ++i)
-        {
-            std::cout << array[i] << ' ';
-        }
+        Utility::outputArray(std::cout, array, size);
+
         std::cout << "\nIds of working markers: ";
-        for (size_t i = 0; i < marker_count; ++i)
-        {
-            if (finish[i] == 0)
-            {
-                std::cout << i + 1 << ' ';
-            }
-        }
-        std::cout << "\n";
-        bool correct = false;
-        int id = -1;
-        while (!correct)
-        {
-            std::cout << "Enter working marker id :\n";
-            std::cin >> id;
-            id--;
-            if (id < 0)
-            {
-                continue;
-            }
-            if (finish[id] == 0)
-            {
-                correct = true;
-            }
-        }
-        std::cout << "Finish marker " << id + 1 << "\n";
+        Utility::outputMarkers(std::cout, finish, markerCount);
+        
+        size_t id = Utility::inputWorkingMarkerId(std::cin, std::cout, finish, size, "Enter working marker id: ");
+        
+        std::cout << "Finishing marker " << id + 1 << "\n";
         finish[id] = 1;
+
+        // set finished marjker event to make him clear the array
         SetEvent(workEvents[id]);
         WaitForSingleObject(markers[id], INFINITE);
+
+        // showing cleared array
         std::cout << "Array:\n";
-        for (size_t i = 0; i < n; ++i)
+        Utility::outputArray(std::cout, array, size);
+        std::cout << "\n";
+
+        // reseting events for other markers to work
+        for (size_t i = 0; i < markerCount; ++i)
         {
-            std::cout << array[i] << ' ';
-        }
-        std::cout << "\n\n";
-        for (size_t i = 0; i < marker_count; ++i)
-        {
-            if (finish[i] == 0)
+            if (finish[i] == 0) // if marker is not finished
             {
                 ResetEvent(events[i]);
                 SetEvent(workEvents[i]);
             }
         }
     }
-    for (size_t i = 0; i < marker_count; ++i)
+
+    // freeing dynamic memory
+    for (size_t i = 0; i < markerCount; ++i)
     {
         CloseHandle(events[i]);
         CloseHandle(markers[i]);
