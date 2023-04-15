08a4b66eab8904b69ddadc2686a2a16964785732
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 20:32:37 +0300

    improve formatting

--- include/struct_args.h
+++ include/struct_args.h
@@ -1,12 +1,12 @@
 #pragma once
-//author: Nikita Kasinski
+// author: Nikita Kasinski
 
 struct MarkerArgs
 {
     size_t id = 0;
     size_t size = 0;
-    int* array = nullptr;
-    const int* finish = nullptr;
+    int *array = nullptr;
+    const int *finish = nullptr;
     CRITICAL_SECTION *iocs = nullptr;
     CRITICAL_SECTION *wcs = nullptr;
 };
