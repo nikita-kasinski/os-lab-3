15885477c36d6cef3d7812bc011d4767ace7837b
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 20:50:45 +0300

    add tests for initializeArray and readPositiveInteger

--- test/test.cc
+++ test/test.cc
@@ -1,2 +1,38 @@
 #include <gtest/gtest.h>
+#include <sstream>
+#include "utility.h"
 
+TEST(TestReadPositiveInteger, TestPositive)
+{
+    std::istringstream in("17");
+    std::ostringstream out;
+    size_t dest;
+    EXPECT_TRUE(Utility::readPositiveInteger(in, out, dest, "", ""));
+    EXPECT_EQ(dest, 17);
+}
+
+TEST(TestReadPositiveInteger, TestNegative)
+{
+    std::istringstream in("-1");
+    std::ostringstream out;
+    size_t dest;
+    EXPECT_FALSE(Utility::readPositiveInteger(in, out, dest, "", ""));
+}
+
+TEST(TestInitializeArray, TestSize1)
+{
+    constexpr size_t size = 1;
+    int *array = Utility::initializeArray(size);
+    EXPECT_EQ(array[0], Utility::valueToInitialize);
+    delete[] array;
+}
+
+TEST(TestInitializeArray, TestSize7)
+{
+    constexpr size_t size = 7;
+    int *array = Utility::initializeArray(size);
+    for(size_t i = 0; i < size; ++i)
+    {
+        EXPECT_EQ(array[i], Utility::valueToInitialize);
+    }
+}
