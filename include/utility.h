025f3faf0ccc5dc310d94da372de694d995b4c56
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-15 20:32:52 +0300

    declare clearArray method

--- include/utility.h
+++ include/utility.h
@@ -21,17 +21,19 @@ public:
         const std::string &inputFailedPrompt);
 
     // outputs array to out
-    static void outputArray(std::ostream &out, const int *array, const size_t& size);
+    static void outputArray(std::ostream &out, const int *array, const size_t &size);
 
     // outputs working markers
-    static void outputMarkers(std::ostream &out, const int *finish, const size_t& markerCount);
+    static void outputMarkers(std::ostream &out, const int *finish, const size_t &markerCount);
 
     // inputs working marker id
     static size_t inputWorkingMarkerId(
-        std::istream &in, 
-        std::ostream &out, 
-        const int* finish, 
-        const size_t& size, 
+        std::istream &in,
+        std::ostream &out,
+        const int *finish,
+        const size_t &size,
         const std::string inputPrompt);
 
+    // reset all positions in the array filled with IdToClear + 1
+    static void clearArray(int *array, const size_t &size, const int IdToClear);
 };
